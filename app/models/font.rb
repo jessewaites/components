cb3b35a0964764e1cc6369b83a33b354667ab3f4
Jesse Waites <jesse.waites@gmail.com>
2013-10-19 19:56:52 -0500

    paperclip round 17

--- app/models/font.rb
+++ app/models/font.rb
@@ -1,7 +1,6 @@
 class Font < ActiveRecord::Base
 	belongs_to :user
     has_reputation :likes, source: :user, aggregated_by: :sum
-    attr_accessible :avatar
 
   # This method associates the attribute ":avatar" with a file attachment
   has_attached_file :image, styles: {
